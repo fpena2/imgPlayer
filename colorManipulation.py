e1ae5291cbd29c830beb7d9fc0ad8b22d2404002
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-18 12:10:41 -0500

    Update and rename Grayscale.py to colorManipulation.py

--- colorManipulation.py
+++ colorManipulation.py
@@ -1,8 +1,5 @@
 from PIL import Image
 import cv2
-#import numpy as np
-#from matplotlib import pyplot as plt
-
 
 #this is a block of code that takes a colored picture
 #converts it to grayscale for better face detection
@@ -11,15 +8,8 @@ def image_to_grayscale(picture):
   gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   return cv2.imshow(picture, gray_image) 
 
-
-image_to_grayscale('testface.jpg')
-
-
-
-
-
-
 #test code--earlier attempts of me trying to code
+#image_to_grayscale('testface.jpg')
 #image = Image.open('testface.jpg')
 #image.show()
 #print(image)
