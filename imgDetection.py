50154ba40bf692ccc7f56ebdc1bbe45d869c43e6
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-27 17:04:55 -0500

    Update imgDetection.py

--- imgDetection.py
+++ imgDetection.py
@@ -1,10 +1,11 @@
 import cv2
+import time
+import numpy as np
 from PIL import ImageFont, ImageDraw, Image
-import numpy
+
 
 def privateDataImg( privateString ):
-  # Font path compartible with linux systems 
-	font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf", 14, encoding="unic")
+	font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf", 14, encoding="unic")
 	fontWidth, fontHeight = font.getsize(privateString)
 
 	privateImgField = Image.new("RGB", (fontWidth + 10, fontHeight + 10) , "white")
@@ -12,5 +13,35 @@ def privateDataImg( privateString ):
 	writePrivateImgField = ImageDraw.Draw(privateImgField)
 	writePrivateImgField.text((5,5) , privateString, "black", font )
 
-	privateImgField.show()
 	privateImgField.save("privateData.png", "PNG")
+
+
+def imgTemplateMatch( templetaImageName, imageToScanName ):
+	# Images will be loading in gray scale
+	imageScan = cv2.imread(imageToScanName, 0)
+
+	templateImage = cv2.imread(templetaImageName,0)
+	templateWidth, templateHight = templateImage.shape[::-1]
+
+	# Use template matching function with method TM_CCOEFF_NORMED
+	result = cv2.matchTemplate(imageScan, templateImage, cv2.TM_CCOEFF_NORMED)
+	minumum, maximum, minLocation, maxLocation = cv2.minMaxLoc(result)
+
+	lowCornerFormula = (maxLocation[0] + templateWidth, maxLocation[1] + templateHight )
+
+	copyimageScan = imageScan.copy()
+
+	cv2.rectangle( copyimageScan , maxLocation, lowCornerFormula, 255, -1 )
+	#copyimageScan.show()
+	cv2.imshow('image',copyimageScan)
+	cv2.waitKey(0)
+
+
+def RUN():
+	userInput = input("Enter private data to protect: ")
+	privateDataImg(userInput)
+
+	imgTemplateMatch("privateData.png", "resume1.png")
+
+
+RUN()
