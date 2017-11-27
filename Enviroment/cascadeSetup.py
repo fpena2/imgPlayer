8535d90d7a656874386044aefc44227e29911ea0
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-27 18:36:52 -0500

    Update cascadeSetup.py

--- Enviroment/cascadeSetup.py
+++ Enviroment/cascadeSetup.py
@@ -1,16 +1,17 @@
-import subprocess
+import os
 
 def createPositiveSamples(inputImg, inputNumb):
 	# Command required by OpenCV to create positives samples 
-	CV2SamplesCommand = "opencv_createsamples -img " + inputImg +" -bg negMap.txt -info positives/info.lst -pngoutput positives -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num " + inputNumb 
-	subprocess.call(list[CV2SamplesCommand])
+	CV2SamplesCommand = "opencv_createsamples -img " + inputImg +" -bg negativesMap.txt -info positives/info.lst -pngoutput positives -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num " + inputNumb
+	os.system(CV2SamplesCommand) 
 
 def createVector(inputNumb):
 	# Creates Vector file needed to create a cascade file 
 	CV2VectorCommand = "opencv_createsamples -info positives/info.lst -num " + inputNumb + " -w 20 -h 20 -vec positives.vec"
-	subprocess.call(list[CV2VectorCommand])
+	os.system(CV2VectorCommand)
 
-def createCascade():
+
+def createCascade(inputNumb):
 	# Creates Cascade file needed for object recognition 
-	CV2CascadeCommand = "opencv_traincascade -data data -vec positives.vec -bg negMap.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20"
-	subprocess.call(list[CV2CascadeCommand])
+	CV2CascadeCommand = "opencv_traincascade -data data -vec positives.vec -bg negativesMap.txt -numPos " + str(inputNumb) +" -numNeg " + str(inputNumb/2) +" -numStages 10 -w 20 -h 20"
+	os.system(CV2CascadeCommand)
