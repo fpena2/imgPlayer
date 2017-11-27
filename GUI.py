97b9477854863fb1219a9a1018f7272c95573f28
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-27 01:02:09 -0500

    Create GUI.py
    Initial implementation of the user interface

---
+++ GUI.py
@@ -0,0 +1,51 @@
+# Requires tkinter installed in Python3
+import tkinter as tk
+
+class userInterface(tk.Frame):
+
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+
+        # Variables
+        self.parent = parent
+        self.stringVariable = tk.StringVar()
+
+        # Functions
+        self.MainWindow()
+
+        # Main Screen Label
+        label = tk.Label(textvariable=self.var, justify = "center")
+        label.pack(anchor="nw")
+
+    # Function to over-ride main label
+    def textArea(self, value= "Select option from menu"):
+        self.stringVariable.set(value)
+
+    def MainWindow(self):
+        # Tittle
+        self.parent.title("Privacy Enhancing Application")
+        self.pack()
+
+        # Menu
+        menuBar = tk.Menu(self.parent)
+        self.parent.config(menu=menuBar)
+
+        # Sub-Menu
+        file = tk.Menu(menuBar, tearoff=0)
+        file.add_command(label="Open", command=None)
+        file.add_command(label="Modify", command=None)
+        file.add_command(label="Save", command=None)
+        file.insert_separator(5)
+        file.add_command(label="Exit", command=self.quit)
+        menuBar.add_cascade(label="File", menu=file)
+
+        # Sub-Menu
+        help = tk.Menu(self.parent, tearoff=0)
+        menuBar.add_cascade(label="Help", menu=help)
+
+
+
+root = tk.Tk()
+root.geometry("600x500")
+app = Hospital(root)
+root.mainloop()
