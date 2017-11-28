b268167fbee15b02123744eb0d74a69c11bb8262
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-27 22:03:28 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,8 +2,9 @@
 import tkinter as tk
 from tempMatch import RUN
 
+
 class userInterface(tk.Frame):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         tk.Frame.__init__(self, parent)
 
         # Variables
@@ -14,11 +15,11 @@ class userInterface(tk.Frame):
         self.MainWindow()
 
         # Main Screen Label
-        label = tk.Label(textvariable=self.stringVariable, justify = "center")
+        label = tk.Label(textvariable=self.stringVariable, justify="center")
         label.pack(anchor="nw")
 
     # Function to over-ride main label
-    def textArea(self, value= "Select option from menu"):
+    def textArea(self, value="Select option from menu"):
         self.stringVariable.set(value)
 
     def MainWindow(self):
@@ -42,26 +43,28 @@ class userInterface(tk.Frame):
         help = tk.Menu(self.parent, tearoff=0)
         menuBar.add_cascade(label="Help", menu=help)
 
-
     def openWindow(self):
         self.window1 = tk.Toplevel(self.parent)
         self.window1.title("Private Information")
         self.window1.geometry("250x150")
 
         # Private Data
-        self.label_find = tk.Label(self.window1,text="Enter private data to secure")
+        self.label_find = tk.Label(
+        self.window1, text="Enter private data to secure")
         self.label_find.pack(anchor="center")
-        self.privateData = tk.Entry(self.window1, bd=5,width= 25)
+        self.privateData = tk.Entry(self.window1, bd=5, width=25)
         self.privateData.pack(anchor="center")
 
         # File
-        self.label_find2 = tk.Label(self.window1,text="Enter document name")
+        self.label_find2 = tk.Label(self.window1, text="Enter document name")
         self.label_find2.pack(anchor="center")
-        self.fileName = tk.Entry(self.window1, bd=5,width= 25)
+        self.fileName = tk.Entry(self.window1, bd=5, width=25)
         self.fileName.pack(anchor="center")
 
         # Enter button
-        self.enterbutton = tk.Button(self.window1, text = "Enter", command = self.sendData )#button command needs to be added
+        # button command needs to be added
+        self.enterbutton = tk.Button(
+        self.window1, text="Enter", command=self.sendData)
         self.enterbutton.pack(anchor="center")
 
 
@@ -69,6 +72,7 @@ class userInterface(tk.Frame):
         privateDataString = self.privateData.get()
         documentFilename = self.fileName.get()
         RUN(privateDataString, documentFilename)
+
 
 
 root = tk.Tk()
