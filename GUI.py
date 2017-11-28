7c522ad349ed97b6197381f60ad5f6e2d5159909
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-27 20:51:10 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,9 +1,9 @@
 # Requires tkinter installed in Python3
 import tkinter as tk
+from tempMatch import RUN
 
 class userInterface(tk.Frame):
-
-    def __init__(self, parent=None):
+    def __init__(self, parent = None):
         tk.Frame.__init__(self, parent)
 
         # Variables
@@ -21,28 +21,6 @@ class userInterface(tk.Frame):
     def textArea(self, value= "Select option from menu"):
         self.stringVariable.set(value)
 
-    def files(self):
-        self.window1 = tk.Toplevel(self.parent)
-        self.window1.geometry("300x150")
-       
-        self.label_find = tk.Label(self.window1,text="Enter specific word here")
-        self.label_find.pack(anchor="center")
-        self.entry1 = tk.Entry(self.window1, bd=5,width= 25)
-        self.entry1.pack(anchor="center")
-
-        self.label_find2 = tk.Label(self.window1,text="Enter JPEG file name here")
-        self.label_find2.pack(anchor="center")
-        self.entry2 = tk.Entry(self.window1, bd=5,width= 25)
-        self.entry2.pack(anchor="center")
-
-        #def enterCommand():
-            #takes the entered values for the jpeg and string
-            #runs them through XML file
-        
-
-        self.enterbutton = tk.Button(self.window1,text = "Enter")#button command needs to be added
-        self.enterbutton.pack(anchor="center")
-
     def MainWindow(self):
         # Title
         self.parent.title("Privacy Enhancing Application")
@@ -54,10 +32,9 @@ class userInterface(tk.Frame):
 
         # Sub-Menu
         file = tk.Menu(menuBar, tearoff=0)
-        file.add_command(label="Open", command=self.files)
-        #file.add_command(label="Modify", command=None)
+        file.add_command(label="Open", command=self.openWindow)
         file.add_command(label="Save", command=None)
-        file.insert_separator(4)
+        file.insert_separator(3)
         file.add_command(label="Exit", command=self.quit)
         menuBar.add_cascade(label="File", menu=file)
 
@@ -65,11 +42,36 @@ class userInterface(tk.Frame):
         help = tk.Menu(self.parent, tearoff=0)
         menuBar.add_cascade(label="Help", menu=help)
 
-    
 
+    def openWindow(self):
+        self.window1 = tk.Toplevel(self.parent)
+        self.window1.title("Private Information")
+        self.window1.geometry("250x150")
+
+        # Private Data
+        self.label_find = tk.Label(self.window1,text="Enter private data to secure")
+        self.label_find.pack(anchor="center")
+        self.privateData = tk.Entry(self.window1, bd=5,width= 25)
+        self.privateData.pack(anchor="center")
+
+        # File
+        self.label_find2 = tk.Label(self.window1,text="Enter document name")
+        self.label_find2.pack(anchor="center")
+        self.fileName = tk.Entry(self.window1, bd=5,width= 25)
+        self.fileName.pack(anchor="center")
+
+        # Enter button
+        self.enterbutton = tk.Button(self.window1, text = "Enter", command = self.sendData )#button command needs to be added
+        self.enterbutton.pack(anchor="center")
+
+
+    def sendData(self):
+        privateDataString = self.privateData.get()
+        documentFilename = self.fileName.get()
+        RUN(privateDataString, documentFilename)
 
 
 root = tk.Tk()
-root.geometry("600x500")
+root.geometry("400x300")
 app = userInterface(root)
 root.mainloop()
