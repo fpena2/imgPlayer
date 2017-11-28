e79b359c2c7e756c52201e93a64b4490595508c4
fpena2 <33009908+fpena2@users.noreply.github.com>
2017-11-28 01:27:34 -0500

    Update enviromentSetup.py

--- Enviroment/enviromentSetup.py
+++ Enviroment/enviromentSetup.py
@@ -14,19 +14,20 @@ def setupFolders():
             userIn = input("The folder " + '"' + folder + '"' +
                            " will be overwritten.  Do you want to continue. (y/n)")
             if (userIn).lower() in ["y", "yes"]:
+				# Used to remove folders even when they contain files
                 shutil.rmtree(folder)
                 os.makedirs(folder)
             else:
                 exit()
 
 
-"""Generates negative images from a source solder"""
+"""Generates 1900 negative images from a source solder"""
 def getAndResizeImages(path="images", imgNumber=1900):
     count = 0
     for imgFile in os.listdir(path):
         # reading images in gray scale
         img = cv2.imread(os.path.join(path, imgFile), 0)
-        imgResize = cv2.resize(img, (100, 100))
+        imgResize = cv2.resize(img, (100, 100)) # resize images to 100x100
         if not os.path.exists("negatives"):
             os.mkdir("negatives")
         cv2.imwrite("negatives/" + str(count) + ".jpg", imgResize)
@@ -44,6 +45,6 @@ def createNegatives():
                 with open("negativesMap.txt", 'a') as handle:
                     handle.write(toWrite)
 
-
+""" Moves the final casacde file from the data folder to the working directory """
 def moveFolder():
     shutil.copy2("./data/cascade.xml", "./generatedCascade.xml")
